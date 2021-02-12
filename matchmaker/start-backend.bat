e51be1d5cb7571d1137db2bf8c43af5ca14ad0a1
prcox <paulreedcox@gmail.com>
2021-02-12 15:00:36 -0800

    changed start-backend.bat

--- matchmaker/start-backend.bat
+++ matchmaker/start-backend.bat
@@ -1,10 +1,10 @@
 cd Backend
-set FLASK_APP=backend.py
-set FLASK_ENV=development
 pip install flask
 pip install flask_cors
 pip install pymongo
 pip install bson
 pip install dnspython
+set FLASK_APP=backend.py
+set FLASK_ENV=development
 cls
 flask run
