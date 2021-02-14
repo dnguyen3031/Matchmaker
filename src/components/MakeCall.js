2e46958a12cb4856a8e83ff09c53214317bda01e
Jonathan Lu <jonathanlu00@gmail.com>
2021-02-14 03:24:23 -0800

    Basic linkage of front and back end

--- src/components/MakeCall.js
+++ src/components/MakeCall.js
@@ -1,40 +1,45 @@
-import axios from 'axios'
-import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-export async function MakeGetCall(person){
-   try {
-      // get character at index 's id number
-      const response = await axios.delete('http://localhost:5000/users/'+person.id);
-      return response;
-   }
-   catch (error) {
-      console.log(error);
-      return false;
-   }
+function MakeCall() {
+
+   return;
+
 }
 
-export async function MakeDeleteCall(person){
-   try {
-      // get character at index 's id number
-      const response = await axios.delete('http://localhost:5000/users/'+person.id);
-      return response;
+   export async function MakeGetCall(person){
+      try {
+         // get character at index 's id number
+         const response = await axios.get('http://127.0.0.1:5000/users');
+         return response.data;
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
    }
-   catch (error) {
-      console.log(error);
-      return false;
-   }
-}
+   
+   // export async function MakeDeleteCall(person){
+   //    try {
+   //       // get character at index 's id number
+   //       const response = await axios.delete('http://localhost:5000/users/'+person.id);
+   //       return response;
+   //    }
+   //    catch (error) {
+   //       console.log(error);
+   //       return false;
+   //    }
+   // }
+   
+   // export async function MakePostCall(person){
+   //    try {
+   //       const response = await axios.post('http://localhost:5000/users', person);
+   //       return response;
+   //    }
+   //    catch (error) {
+   //       console.log(error);
+   //       return false;
+   //    }
+   // }
 
-export async function MakePostCall(person){
-   try {
-      const response = await axios.post('http://localhost:5000/users', person);
-      return response;
-   }
-   catch (error) {
-      console.log(error);
-      return false;
-   }
-}
 
-
-export default MakeGetCall;
+export default MakeCall;
--- src/components/pages/ProfilePage.js
+++ src/components/pages/ProfilePage.js
@@ -1,9 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import {Row, Col, Container, Table, Card, Button} from 'react-bootstrap';
 import CustomNavbar from '../../CustomNavbar';
-import MakeGetCall from '../MakeCall';
+import axios from 'axios';
 
 function ProfilePage(props) {
+   const [user, setUser] = useState([]);
+
+   useEffect(() => {
+      fetchUser(props.id).then( result => {
+         if (result)
+            setUser(result);
+      });
+   }, []);
+
+   async function fetchUser(id){
+      try {
+         // get character at index 's id number
+         const response = await axios.get('http://127.0.0.1:5000/users/' + id);
+         console.log(response.data);
+         console.log("YOOOOOOO");
+         return response.data;
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+   }
+
    return <div> 
       <CustomNavbar />
       <Container fluid>
@@ -11,28 +34,42 @@ function ProfilePage(props) {
             <Col /> 
             {/*2 empty cols ensure middle col is centered */}
             <Col> 
-               <ProfileCard />
+            <Card style={{ width: '18rem' }}>
+               <Card.Img variant="top" src="testProfilePic.JPG" />
+               <Card.Body>
+                  <Card.Title>{user.name}</Card.Title>
+                  <Card.Text>
+                     {user.bio}
+                  </Card.Text>
+                  {/* <Card.Text>
+                     Discord: {user.contact_info.discord}
+                  </Card.Text>
+                  <Card.Text>
+                     Email: {user.contact_info.email}
+                  </Card.Text> */}
+                  <Button variant="primary">Edit Profile</Button>
+               </Card.Body>
+               </Card>
             </Col>
             <Col /> 
          </Row>
          <Row>
             <Col>
-               <GamesTable />
+               <GamesTable user={user} />
             </Col>
          </Row>
       </Container>
    </div>;
 }
 
-function ProfileCard() {
+function ProfileCard(props) {
    return (
    <Card style={{ width: '18rem' }}>
      <Card.Img variant="top" src="holder.js/100px180" />
      <Card.Body>
-       <Card.Title>Card Title</Card.Title>
+       <Card.Title>{props.name}</Card.Title>
        <Card.Text>
-         Some quick example text to build on the card title and make up the bulk of
-         the card's content.
+         {props.bio}
        </Card.Text>
        <Button variant="primary">Go somewhere</Button>
      </Card.Body>
@@ -45,29 +82,18 @@ function GamesTable() {
       <Table striped bordered hover>
         <thead>
           <tr>
-            <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Game Title</th>
+            <th>Rating</th>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Team Fortress 2</td>
+            <td>1231</td>
           </tr>
           <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td colSpan="2">Larry the Bird</td>
-            <td>@twitter</td>
+            <td>Raid Shadow Legends</td>
+            <td> > 9000</td>
           </tr>
         </tbody>
       </Table>
