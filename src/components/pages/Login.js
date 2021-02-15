2b60719fca8691a4a2a6c0da4720dd69de176580
Max Christy <47064300+mchris25@users.noreply.github.com>
2021-02-15 15:07:13 -0800

    Update Login.js

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import {
+  Button, Container, Row, Col, Form, FormControl, FormGroup, Nav, Navbar, NavItem, NavLink
+} from 'react-bootstrap';
+import CustomNavbar from '../../CustomNavbar';
 
 function Login() {
-   return <h2>Login</h2>;
+  return <div> 
+    <CustomNavbar />
+    <Container className="justify-content-md-center">
+      <Form>
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        
+        <Container>
+          <Row>
+            <Col>
+              <Button block variant="primary" type="submit">
+                Login
+              </Button>
+            </Col>
+            <Col>
+              <Button block variant="primary" type="submit" href="/create-account">
+                Create Account
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+
+      </Form>
+    </Container>
+  </div>;
  }
  
 
