7a8981558c90cbf6950492a55549ada76c3c8e18
Daniel Nguyen <dnguy303@calpoly.edu>
2021-03-10 14:34:33 -0800

    Update Matchmaking.js

--- src/components/pages/Matchmaking.js
+++ src/components/pages/Matchmaking.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {
+   Button, Container, Row, Col, Form, FormControl, FormGroup, Nav, Navbar, NavItem, NavLink, Alert, Modal, Dropdown, DropdownButton
+ } from 'react-bootstrap';
 import CustomNavbar from '../CustomNavbar';
 
+function getNumGames() {
+   return 3;
+}
+
 function Matchmaking() {
-   return <div> 
+   return <> 
       <CustomNavbar />
       <h2>matchmaking</h2>
-   </div>;
+      <Container className="justify-content-md-center">
+         <Dropdown>
+         </Dropdown>
+         <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item href="#/action-1">Krunker</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Minecraft</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">idk</Dropdown.Item>
+         </DropdownButton>
+
+         <Button variant="outline-primary">Add New Game</Button>{' '}
+      </Container>
+   </>;
  }
  
 export default Matchmaking;
