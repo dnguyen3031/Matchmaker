7674bcd4027f4acf3216d9d92462c6c28ad48e25
Xander Wallace <xhw2000@gmail.com>
2021-05-22 11:50:39 -0700

    Added framework for leaving queue

--- src/components/pages/Queue.js
+++ src/components/pages/Queue.js
@@ -1,20 +1,62 @@
 import React from 'react'
 import CustomNavbar from '../CustomNavbar'
-import { Col, Row, Container } from 'react-bootstrap'
+import { Button, Col, Row, Container } from 'react-bootstrap'
 import './PageTemplate.css'
 import FriendBar from '../FriendBar'
+import axios from 'axios'
 import Lobby from './Lobby'
 
+/* eslint-disable */
+
 function Queue (props) {
-  if (props.data.user.lobby) { return <Lobby data={props.data} setToken={props.setToken} fetchData={props.fetchData} setData={props.setData} MatchmakingDisplay={props.MatchmakingDisplay}/> }
+  if (props.data.user.lobby) {
+    return <Lobby data={props.data} setToken={props.setToken}
+   fetchData={props.fetchData} setData={props.setData} MatchmakingDisplay={props.MatchmakingDisplay}/>
+  }
 
   async function checkForUpdates () {
-    props.fetchData({ id: props.data.id, get_group: true, get_lobby: true, get_game: true, current_page: props.pageName }).then(result => {
+    props.fetchData({
+      id: props.data.id,
+      get_group: true,
+      get_lobby: true,
+      get_game: true,
+      current_page: props.pageName
+    }).then(result => {
       console.log('fetched data')
       props.setData(result)
     })
   }
+  async function makePatchCallGames () {
+    try {
+      return await axios.patch('http://localhost:5000/games/' + props.data.lobby._id, change)
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
 
+  async function makePatchCallUsers (change) {
+    try {
+      return await axios.patch('http://localhost:5000/users/' + props.data.id, change)
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  }
+
+  function submitLeave () {
+    const inQueueObj = {
+      in_queue: false
+    }
+    makePatchCallUsers(inQueueObj)
+    /*
+    find group id
+    for each lobby in queue
+      for each group in lobby
+        remove group with that id
+    patch new queue
+    */
+  }
   setTimeout(() => { checkForUpdates() }, 1000)
 
   return <div>
@@ -27,6 +69,9 @@ function Queue (props) {
             <Col>
               <h2 style={{ color: 'white' }}> finding match...</h2>
               <h3 style={{ color: 'white' }}> please wait</h3>
+              <Button variant="primary" type="button" onClick ={submitLeave}>
+                Leave Queue
+              </Button>
             </Col>
             <Col md={3}>
               <FriendBar data={props.data}/>
