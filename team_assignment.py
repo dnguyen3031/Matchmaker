b553f9cbefab73c7bf3c4b8db0ca264e3ab90871
Xander Wallace <xhw2000@gmail.com>
2021-05-31 00:36:49 -0700

    completed find_all_matches

--- team_assignment.py
+++ team_assignment.py
@@ -1,5 +1,6 @@
 from mongodb import Game
 from suitable_lobby import get_team_combos
+from itertools import combinations
 
 def get_group_sizes(groups):
     """returns a list of of ints corresponding to the group sizes of the lobby"""
@@ -62,40 +63,78 @@ def find_all_groups_with_size(target_group_size, lobby):
             groups_with_size.append(group)
     return groups_with_size
 
+def make_team(group, rest_of_team):
+    possible_team = [group]
+    for other_group in rest_of_team:
+        possible_team.append(other_group)
+    return possible_team
+
 def find_all_teams_with_template(team_template, groups):
+    """recursively builds teams with the given template. Does not modify groups"""
     if len(team_template) == 0:
         return [[]]
     teams = []
-    for target_group_size in team_template:
-        print(target_group_size)
-        group_candidates = find_all_groups_with_size(target_group_size, groups)
-        for group in group_candidates:
-            print("group",group)
-            remaining_template = list(team_template)
-            remaining_template.remove(target_group_size)
-            remaining_groups = list(groups)
-            remaining_groups.remove(group)
-            print("recursing...")
-            rest_of_team_possibilities = find_all_teams_with_template(remaining_template, remaining_groups)
-            print("returning. found:", rest_of_team_possibilities)
-            for rest_of_team in rest_of_team_possibilities:
-                possible_team= list(rest_of_team)
-                possible_team.append(group)
-                teams.append(possible_team)
+    target_group_size = team_template[0]
+    group_candidates = find_all_groups_with_size(target_group_size, groups)
+    groups_copy = list(groups)
+    while len(group_candidates) > 0:
+        group = group_candidates[0]
+        remaining_template = list(team_template)
+        remaining_template.remove(target_group_size)
+        remaining_groups = list(groups_copy)
+        remaining_groups.remove(group)
+        rest_of_team_possibilities = find_all_teams_with_template(remaining_template, remaining_groups)
+        for rest_of_team in rest_of_team_possibilities:
+            possible_team = make_team(group, rest_of_team)
+            teams.append(possible_team)
+        group_candidates.remove(group)
+        groups_copy.remove(group)
     return teams
 
-def find_all_matches_with_template(match_template, groups):
-    matches = []
-    for team_template in match_template:
-        find_all_teams_with_template(team_template, groups)
+def find_all_teams(team_templates, groups):
+    teams = []
+    for team_template in team_templates:
+        teams += find_all_teams_with_template(team_template, groups)
+    return teams
 
-    return matches
+def print_team(team):
+    for group in team:
+        print(group["players"], end="")
+    print()
 
-def find_all_matches(match_templates, lobby):
-    matches = []
-    for match_template in match_templates:
-        groups = list(lobby["groups"])
-        matches += find_all_matches_with_template(match_template, groups)
+def print_teams(teams):
+    for team in teams:
+        print(" [", end ="")
+        for group in team:
+            print(group["players"], end="")
+        print("] ", end ="")
+    print()
+
+def clean_self_vs_self(matches):
+    """removes matches where a player is on multiple opposing teams"""
+    i = 0
+    while i < len(matches):
+        for team in matches[i]:
+            found_dup = False
+            other_teams = list(matches[i])
+            other_teams.remove(team)
+            for group in team:
+                for other_team in other_teams:
+                    if group in other_team:
+                        del matches[i]
+                        i -= 1
+                        found_dup = True
+                        break
+                if found_dup:
+                    break
+            if found_dup:
+                break
+        i += 1
+
+def find_all_matches(team_templates, lobby, num_teams):
+    teams = find_all_teams(team_templates, lobby["groups"])
+    matches = list(combinations(teams, num_teams))
+    clean_self_vs_self(matches)
     return matches
 
 def choose_best_match(all_matches):
@@ -110,6 +149,7 @@ def assign_teams(full_lobby, num_teams, players_per_team):
     full_lobby["groups"].sort(key=group_size)
     group_sizes = get_group_sizes(full_lobby["groups"])
     match_templates = get_match_templates(group_sizes, players_per_team, num_teams)
-    all_matches = find_all_matches(match_templates, full_lobby)
+    team_templates = get_team_combos(group_sizes, players_per_team)
+    all_matches = find_all_matches(team_templates, full_lobby, num_teams)
     chosen_match = choose_best_match(all_matches)
     full_lobby["teams"] = chosen_match
--- team_assignment_tests.py
+++ team_assignment_tests.py
@@ -1,31 +1,40 @@
 import unittest
 from team_assignment import *
 
+def simplify_teams(teams):
+    """for simplifying testing output. Turns a full json of teams into a simplified list representation"""
+    teams_names  = []
+    for team in teams:
+        team_names = []
+        for group in team:
+            players = []
+            for player_key in group["players"].keys():
+                players.append(group["players"][player_key])
+            team_names.append(players)
+        teams_names.append(team_names)
+    return teams_names
+
+def simplify_matches(matches):
+    """for simplifying testing output. Turns a combination iterable of matches into a simplified list representation"""
+    simple_matches  = []
+    for match in matches:
+        simple_matches.append(simplify_teams(match))
+    return simple_matches
+
+def jsonify_group(simple_groups):
+    """for simplifying testing input. Turns a simplified list representation of groups into a list of group jsons"""
+    arbitrary_increment = 2
+    key = 0
+    groups = []
+    for simple_group in simple_groups:
+        group = {"players": {}, "num_players": len(simple_group), "id":str(key+1)}
+        for player in simple_group:
+            key += arbitrary_increment
+            group["players"][str(key)] = player
+        groups.append(group)
+    return groups
 
 class MyTestCase(unittest.TestCase):
-
-    # def test_find_best_teams(self):
-    #     groups = [
-    #                 {
-    #                     "_id": "609cc564b0105c43e02ba04f",
-    #                     "num_players": 2,
-    #                     "players": {
-    #                         "6024098ac9b27e9f9995df97": "Template",
-    #                         "607228229a8f71adf99cbfbe": "max"
-    #                     }
-    #                 },
-    #                 {
-    #                     "_id": "60a1861c2d951ae021f05d60",
-    #                     "num_players": 2,
-    #                     "players": {
-    #                         "607228229a8f71adf99cbfbe": "max",
-    #                         "6024098ac9b27e9f9995df97": "Template"
-    #                     }
-    #                 }
-    #             ]
-    #     t = find_best_teams(groups, 2 , 1)
-    #
-    #     self.assertEqual(t, [])
 
     def test_get_match_templates_1(self):
         groups = [1,1,1,1,2,2]
@@ -49,7 +58,7 @@ class MyTestCase(unittest.TestCase):
                          [[[1, 1, 1, 1], [2, 2], [1, 3], [4], [4]],
                           [[1, 1, 2], [1, 1, 2], [1, 3], [4], [4]]])
 
-    def test_find_teams_with_template_1(self):
+    def test_find_teams_with_template_actual_groups(self):
         groups =  [
                 {
                     "_id": "609719c646fabbf5e1931578",
@@ -85,16 +94,149 @@ class MyTestCase(unittest.TestCase):
                     "_id": "60a1861c2d951ae021f05d64",
                     "num_players": 2,
                     "players": {
-                        "60a5f2fa05f1815bd50e4522": "gayden",
+                        "60a5f2fa05f1815bd50e4522": "hayden",
                         "60a5fec405f1815bd50e452b": "dad"
                     }
                 }
         ]
         template = [1,1,2]
         teams = find_all_teams_with_template(template, groups)
-        self.assertEqual(teams, [])
+        teams_names = simplify_teams(teams)
+        self.assertEqual(teams_names,
+                         [[["mikescott0808"], ["alexd1214"], ["template", "max"]],
+                          [["mikescott0808"], ["alexd1214"], ["braden", "sadfasfas"]],
+                          [["mikescott0808"], ["alexd1214"], ["hayden", "dad"]]] )
+
+    def test_find_teams_with_template_1(self):
+        simple_groups = [["mikeScott0808"], ["alexd1214"],
+                         ["template", "max"], ["braden", "sadfasfas"], ["hayden", "dad"]]
+        groups = jsonify_group(simple_groups)
+        template = [1,1,2]
+        teams = find_all_teams_with_template(template, groups)
+        teams_names = simplify_teams(teams)
+        self.assertEqual(teams_names,
+                         [[["mikeScott0808"], ["alexd1214"], ["template", "max"]],
+                          [["mikeScott0808"], ["alexd1214"], ["braden", "sadfasfas"]],
+                          [["mikeScott0808"], ["alexd1214"], ["hayden", "dad"]]] )
+
+    def test_find_teams_with_template_2(self):
+        simple_groups = [["mike"], ["alex"],
+                         ["template", "max"], ["braden", "sadfas"], ["hayden", "dad"]]
+        groups = jsonify_group(simple_groups)
+        template = [2,2]
+        teams = find_all_teams_with_template(template, groups)
+        teams_names = simplify_teams(teams)
+        self.assertEqual(teams_names,
+                         [[["template", "max"], ["braden", "sadfas"]],
+                          [["template", "max"], ["hayden", "dad"]],
+                          [["braden", "sadfas"], ["hayden", "dad"]]])
 
 
+    def test_find_teams_with_template_3(self):
+        simple_groups = [["mike"], ["alex"], ["brad"], ["bill"],
+                         ["template", "max"], ["braden", "sadfas"], ["hayden", "dad"], ["bro", "bob"]]
+        groups = jsonify_group(simple_groups)
+        template = [1,1,2,2]
+        teams = find_all_teams_with_template(template, groups)
+        teams_names = simplify_teams(teams)
+        self.assertEqual(teams_names,
+                         [[['mike'], ['alex'], ['template', 'max'], ['braden', 'sadfas']],
+                          [['mike'], ['alex'], ['template', 'max'], ['hayden', 'dad']],
+                          [['mike'], ['alex'], ['template', 'max'], ['bro', 'bob']],
+                          [['mike'], ['alex'], ['braden', 'sadfas'], ['hayden', 'dad']],
+                          [['mike'], ['alex'], ['braden', 'sadfas'], ['bro', 'bob']],
+                          [['mike'], ['alex'], ['hayden', 'dad'], ['bro', 'bob']],
+                          [['mike'], ['brad'], ['template', 'max'], ['braden', 'sadfas']],
+                          [['mike'], ['brad'], ['template', 'max'], ['hayden', 'dad']],
+                          [['mike'], ['brad'], ['template', 'max'], ['bro', 'bob']],
+                          [['mike'], ['brad'], ['braden', 'sadfas'], ['hayden', 'dad']],
+                          [['mike'], ['brad'], ['braden', 'sadfas'], ['bro', 'bob']],
+                          [['mike'], ['brad'], ['hayden', 'dad'], ['bro', 'bob']],
+                          [['mike'], ['bill'], ['template', 'max'], ['braden', 'sadfas']],
+                          [['mike'], ['bill'], ['template', 'max'], ['hayden', 'dad']],
+                          [['mike'], ['bill'], ['template', 'max'], ['bro', 'bob']],
+                          [['mike'], ['bill'], ['braden', 'sadfas'], ['hayden', 'dad']],
+                          [['mike'], ['bill'], ['braden', 'sadfas'], ['bro', 'bob']],
+                          [['mike'], ['bill'], ['hayden', 'dad'], ['bro', 'bob']],
+                          [['alex'], ['brad'], ['template', 'max'], ['braden', 'sadfas']],
+                          [['alex'], ['brad'], ['template', 'max'], ['hayden', 'dad']],
+                          [['alex'], ['brad'], ['template', 'max'], ['bro', 'bob']],
+                          [['alex'], ['brad'], ['braden', 'sadfas'], ['hayden', 'dad']],
+                          [['alex'], ['brad'], ['braden', 'sadfas'], ['bro', 'bob']],
+                          [['alex'], ['brad'], ['hayden', 'dad'], ['bro', 'bob']],
+                          [['alex'], ['bill'], ['template', 'max'], ['braden', 'sadfas']],
+                          [['alex'], ['bill'], ['template', 'max'], ['hayden', 'dad']],
+                          [['alex'], ['bill'], ['template', 'max'], ['bro', 'bob']],
+                          [['alex'], ['bill'], ['braden', 'sadfas'], ['hayden', 'dad']],
+                          [['alex'], ['bill'], ['braden', 'sadfas'], ['bro', 'bob']],
+                          [['alex'], ['bill'], ['hayden', 'dad'], ['bro', 'bob']],
+                          [['brad'], ['bill'], ['template', 'max'], ['braden', 'sadfas']],
+                          [['brad'], ['bill'], ['template', 'max'], ['hayden', 'dad']],
+                          [['brad'], ['bill'], ['template', 'max'], ['bro', 'bob']],
+                          [['brad'], ['bill'], ['braden', 'sadfas'], ['hayden', 'dad']],
+                          [['brad'], ['bill'], ['braden', 'sadfas'], ['bro', 'bob']],
+                          [['brad'], ['bill'], ['hayden', 'dad'], ['bro', 'bob']]])
+
+    def test_find_all_matches_1(self):
+        team_templates = [[1, 2], [1,1,1]]
+        lobby  = {"groups": jsonify_group([["mike"], ["alex"], ["brad"], ["bill"], ["template", "max"]])}
+        all_matches = find_all_matches(team_templates, lobby, 2)
+        # for match in all_matches:
+        #     print(match)
+        simple_matches = simplify_matches(all_matches)
+        self.assertEqual(simple_matches ,
+                         [[[['mike'], ['template', 'max']], [['alex'], ['brad'], ['bill']]],
+                          [[['alex'], ['template', 'max']], [['mike'], ['brad'], ['bill']]],
+                          [[['brad'], ['template', 'max']], [['mike'], ['alex'], ['bill']]],
+                          [[['bill'], ['template', 'max']], [['mike'], ['alex'], ['brad']]]]
+                         )
+
+    def test_find_all_matches_2(self):
+        team_templates = [[1, 1, 1, 1], [1,1,2], [2,2]]
+        lobby  = {"groups": jsonify_group([["mike"], ["alex"], ["brad"], ["bill"], ["template", "max"], ["bro", "bob"]])}
+        all_matches = find_all_matches(team_templates, lobby, 2)
+        # for match in all_matches:
+        #     print(match)
+        simple_matches = simplify_matches(all_matches)
+        self.assertEqual(simple_matches ,
+            [[[['mike'], ['alex'], ['brad'], ['bill']],
+              [['template', 'max'], ['bro', 'bob']]],
+             [[['mike'], ['alex'], ['template', 'max']],
+              [['brad'], ['bill'], ['bro', 'bob']]],
+             [[['mike'], ['alex'], ['bro', 'bob']],
+              [['brad'], ['bill'], ['template', 'max']]],
+             [[['mike'], ['brad'], ['template', 'max']],
+              [['alex'], ['bill'], ['bro', 'bob']]],
+             [[['mike'], ['brad'], ['bro', 'bob']],
+              [['alex'], ['bill'], ['template', 'max']]],
+             [[['mike'], ['bill'], ['template', 'max']],
+              [['alex'], ['brad'], ['bro', 'bob']]],
+             [[['mike'], ['bill'], ['bro', 'bob']],
+              [['alex'], ['brad'], ['template', 'max']]]]
+        )
+    def test_find_all_matches_3(self):
+        team_templates = [[1, 1], [2]]
+        lobby  = {"groups": jsonify_group([["mike"], ["alex"], ["brad"], ["bill"], ["template", "max"]])}
+        all_matches = find_all_matches(team_templates, lobby, 3)
+        # for match in all_matches:
+        #     print(match)
+        simple_matches = simplify_matches(all_matches)
+        self.assertEqual(simple_matches ,
+            [[[['mike'], ['alex']], [['brad'], ['bill']], [['template', 'max']]],
+             [[['mike'], ['brad']], [['alex'], ['bill']], [['template', 'max']]],
+             [[['mike'], ['bill']], [['alex'], ['brad']], [['template', 'max']]]]
+        )
+
+    def test_find_all_matches_4(self):
+        team_templates = [[1, 1], [2]]
+        lobby  = {"groups": jsonify_group([["mike"], ["alex"], ["template", "max"], ["bro", "bob"]])}
+        all_matches = find_all_matches(team_templates, lobby, 3)
+        # for match in all_matches:
+        #     print(match)
+        simple_matches = simplify_matches(all_matches)
+        self.assertEqual(simple_matches ,
+            [[[['mike'], ['alex']], [['template', 'max']], [['bro', 'bob']]]]
+        )
 
     # def test_fit_first(self):
     #     lobby ={
@@ -292,5 +434,5 @@ class MyTestCase(unittest.TestCase):
     #      ])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
